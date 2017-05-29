f6cd4de8c285510d46bcfe76cacdfc2b052d0da3
g1st <gintstan@gmail.com>
2017-05-29 19:57:20 +0100

    added reverse engeneered index page

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const express = require('express'),
       // PORT = (process.ENV.PORT || 3000);
       PORT = 3000;
 
-//view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -23,17 +23,20 @@ app.set('view engine', 'pug');
 
 // homepage
 app.get('/', function(req, res) {
-  res.send('Welcome to Timestamp Microservice Hompepage!');
+  res.render('index', {title: 'Timestamp Microservice'});
 });
 
 // listening to all queries
 app.get('*/:q', function(req, res) {
+  console.log(req.params);
   const q = req.params.q;
+
   const json_date = parseQuery(q);
   res.send(json_date);
 });
 
 /* ====================== RUN APP ========================= */
+
 app.listen(PORT, function(){
   console.log(`app is listening on port ${PORT}`);
 });
@@ -41,26 +44,21 @@ app.listen(PORT, function(){
 /* ====================== FUNCTIONS ======================= */
 
 function parseQuery(q) {
+  console.log(q);
   let result = {
     unix: null,
     natural: null
   };
 
   // query is a number
-  if (Number(q)) {
-    // const date = new Date(parseInt(q)),
-    //       month_day = date.toLocaleString('en-US', {month: 'long', day: 'numeric'}),
-    //       year = date.toLocaleString('en-US', {year: 'numeric'}),
-    //
-    //       natural_date = month_day + ', ' + year;
-
+  if (Number(q) || q === '0') {
     result.unix = parseInt(q);
     result.natural = unixtonatural(parseInt(q));
     return result;
   }
 
   // query is a date string
-  if (Date.parse(q)) {
+  if (Date.parse(q) + 1) {
     result.unix = Date.parse(q);
     result.natural = unixtonatural(Date.parse(q));
     return result;
